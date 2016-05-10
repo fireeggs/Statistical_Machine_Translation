c373c698b749a1fc49a99239c5610194b58235e4
Sean Kim <seungkyu.sean.kim@gmail.com>
2016-05-09 23:50:49 -0400

    Compute N-gram counts

---
+++ lm_train.m
@@ -0,0 +1,107 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                               %
+% CSC401. Statistical Machine Translation                       %
+%                                                               %
+% Assignemnt 2.                                                 %
+% part2. Compute N-gram Counts                                  %
+%                                                               %
+% lm_train.m                                                    %
+%                                                               %
+% Created by Seungkyu Kim on Mar 10th, 2016                     %
+% Copyright 2016 Seungkyu Kim All rights reserved.              %
+%                                                               %
+%                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function LM = lm_train(dataDir, language, fn_LM)
+%
+%  lm_train
+% 
+%  This function reads data from dataDir, computes unigram and bigram counts,
+%  and writes the result to fn_LM
+%
+%  INPUTS:
+%
+%       dataDir     : (directory name) The top-level directory containing 
+%                                      data from which to train or decode
+%                                      e.g., '/u/cs401/A2_SMT/data/Toy/'
+%       language    : (string) either 'e' for English or 'f' for French
+%       fn_LM       : (filename) the location to save the language model,
+%                                once trained
+%  OUTPUT:
+%
+%       LM          : (variable) a specialized language model structure  
+%
+%  The file fn_LM must contain the data structure called 'LM', 
+%  which is a structure having two fields: 'uni' and 'bi', each of which holds
+%  sub-structures which incorporate unigram or bigram COUNTS,
+%
+%       e.g., LM.uni.word = 5       % the word 'word' appears 5 times
+%             LM.bi.word.bird = 2   % the bigram 'word bird' appears twice
+
+
+global CSC401_A2_DEFNS
+
+LM=struct();
+LM.uni = struct();
+LM.bi = struct();
+
+SENTSTARTMARK = 'SENTSTART'; 
+SENTENDMARK = 'SENTEND';
+
+DD = dir( [ dataDir, filesep, '*', language] );
+
+disp([ dataDir, filesep, '.*', language] );
+
+for iFile=1:length(DD)
+
+  lines = textread([dataDir, filesep, DD(iFile).name], '%s','delimiter','\n');
+
+  for l=1:length(lines)
+
+    processedLine =  preprocess(lines{l}, language);
+    words = strsplit(processedLine, ' ' );
+
+	for i=1:length(words)
+		
+		% iterate up to the second last word from the words
+		if i ~= length(words)
+
+			% check word exists in unigram, add if it new word
+			if ~isfield( LM.uni, words{i} )
+				LM.uni.( words{i} ) = 1;
+		
+			% word exists, so add count +1
+			else
+				LM.uni.( words{i} ) = LM.uni.( words{i} ) + 1;
+			end
+
+			% check wrod exists in bigram, add if it new word
+			if ~isfield( LM.bi, words{i} )
+				LM.bi.( words{i} ) = struct();
+			end
+
+			% check follwed word is exists, add if it is new bigram
+			if ~isfield(LM.bi.( words{i} ), words{ i+1 } )
+				LM.bi.( words{i} ).( words{ i+1 } ) = 1;
+		
+			% bigram already exists, so add count + 1
+			else
+				LM.bi.( words{i} ).( words{ i+1 } ) = LM.bi.( words{i} ).( words{ i+1 } ) + 1;
+			end
+		
+		% add last word to unigram from words
+		else
+			if ~isfield( LM.uni, words{end} )
+				LM.uni.( words{end} ) = 1;
+			else
+				LM.uni.( words{end} ) = LM.uni.( words{end} ) + 1;
+			end
+				
+	
+		end
+	end
+  end
+end
+
+save( fn_LM, 'LM', '-mat'); 
